5435c27f1c7970acdcd1311a8ccc81585c0c6c4f
Kazi tanvir Azad <kazitanvir.azad@publicissapient.com>
2022-07-01 00:26:15 +0530

    selection sort pushed

---
+++ DataStructuresJava/SortAlgorithms/src/com/sort/selectionSort/SelectionSort2.java
@@ -0,0 +1,37 @@
+package com.sort.selectionSort;
+
+public class SelectionSort2 {
+
+	public static void main(String[] args) {
+		int[] unsortedArray = { 22, -1, 55, 43, 35, 5, 8, 7, 11, 6, 99, 55 };
+		printArray(unsortedArray);
+		selectionSort(unsortedArray);
+		printArray(unsortedArray);
+	}
+
+	public static int[] selectionSort(int[] arr) {
+		for (int i = arr.length - 1; i >= 0; i--) {
+			int largest = 0;
+			int lastIndex = i;
+			for (int j = 0; j <= i; j++) {
+				if (arr[j] > arr[largest]) {
+					largest = j;
+				}
+			}
+			swap(arr, lastIndex, largest);
+		}
+		return arr;
+	}
+
+	public static void swap(int[] arr, int lastIndex, int largest) {
+		int temp = arr[lastIndex];
+		arr[lastIndex] = arr[largest];
+		arr[largest] = temp;
+	}
+
+	public static void printArray(int[] arr) {
+		for (int i : arr)
+			System.out.print(i + " ");
+		System.out.println();
+	}
+}
