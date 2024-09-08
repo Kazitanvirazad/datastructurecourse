eb6b67bcfa41537144d6fc1a31f5eb11acd69629
Kazitanvirazad <tanvir.kaziazad@gmail.com>
2024-09-09 00:20:27 +0530

    Binary Heap dsa and course compeltion certification commit

--- DataStructuresJava/BinaryHeap/src/main/java/com/learndsa/heap/Heap.java
+++ DataStructuresJava/BinaryHeap/src/main/java/com/learndsa/heap/Heap.java
@@ -1,11 +1,21 @@
 package com.learndsa.heap;
 
+/*
+ * Max Heap
+ */
 public class Heap {
     private int[] heap;
     private int size;
 
     public Heap(int capacity) {
         this.heap = new int[capacity];
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("Heap is empty!");
+        }
+        return heap[0];
     }
 
     public void insert(int value) {
@@ -17,6 +27,63 @@ public class Heap {
         size++;
     }
 
+    public int delete(int index) {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("Heap is empty!");
+        }
+        int parent = getParentIndex(index);
+        int deletedValue = heap[index];
+
+        heap[index] = heap[size - 1];
+
+        if (index == 0 || heap[index] < heap[parent]) {
+            fixHeapBelow(index, size - 1);
+        } else {
+            fixHeapAbove(index);
+        }
+
+        size--;
+        return deletedValue;
+    }
+
+    public void sort() {
+        int lastHeapIndex = size - 1;
+
+        for (int i = 0; i < lastHeapIndex; i++) {
+            int temp = heap[0];
+            heap[0] = heap[lastHeapIndex - i];
+            heap[lastHeapIndex - i] = temp;
+
+            fixHeapBelow(0, lastHeapIndex - i - 1);
+        }
+    }
+
+    private void fixHeapBelow(int index, int lastHeapIndex) {
+        int childToSwap;
+
+        while (index <= lastHeapIndex) {
+            int leftChild = getChild(index, true);
+            int rightChild = getChild(index, false);
+            if (leftChild <= lastHeapIndex) {
+                if (rightChild > lastHeapIndex) {
+                    childToSwap = leftChild;
+                } else {
+                    childToSwap = (heap[leftChild] > heap[rightChild] ? leftChild : rightChild);
+                }
+                if (heap[index] < heap[childToSwap]) {
+                    int temp = heap[index];
+                    heap[index] = heap[childToSwap];
+                    heap[childToSwap] = temp;
+                } else {
+                    break;
+                }
+                index = childToSwap;
+            } else {
+                break;
+            }
+        }
+    }
+
     private void fixHeapAbove(int index) {
         int newValue = heap[index];
         while (index > 0 && newValue > heap[getParentIndex(index)]) {
@@ -26,8 +93,23 @@ public class Heap {
         heap[index] = newValue;
     }
 
+    public void printHeap() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(heap[i] + " ");
+        }
+        System.out.println();
+    }
+
     private boolean isFull() {
         return size == heap.length;
+    }
+
+    private boolean isEmpty() {
+        return size == 0;
+    }
+
+    private int getChild(int index, boolean left) {
+        return 2 * index + (left ? 1 : 2);
     }
 
     private int getParentIndex(int index) {
